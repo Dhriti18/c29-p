370b0bb37ab994e9af75e7b59365f33c7bc468e8
Dhriti18 <73419650+Dhriti18@users.noreply.github.com>
2021-04-03 20:45:02 +0530

    Add files via upload

---
+++ Polygon.js
@@ -0,0 +1,20 @@
+
+
+class Polygon{
+    constructor(x, y, width, height) {
+        var options = {
+             isStatic:true,        
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.image=loadImage("polygon.png")
+        World.add(world, this.body);
+      }
+      display(){
+        
+        imageMode(CENTER);
+       image(this.image,this.body.position.x,this.body.position.y,this.width, this.height);
+     
+      }
+}
